93c1ea035b46614fd907e33303c6a876d32e2c78
Peter Hutterer <peter.hutterer@redhat.com>
2008-09-26 09:37:48 +0930

    Remove default properties (XI_PROP_MODE, XI_PROP_ENABLED)
    These should be defined by the server, not the protocol.

--- XI.h
+++ XI.h
@@ -149,11 +149,6 @@ SOFTWARE.
 #define XI_FOOTMOUSE	"FOOTMOUSE"
 #define XI_JOYSTICK	"JOYSTICK"
 
-/* Properties understood by the server */
-#define XI_PROP_ENABLED      "Device Enabled"
-#define XI_PROP_MODE         "Device Mode"
-
-
 #define Dont_Check			0
 #define XInput_Initial_Release		1
 #define XInput_Add_XDeviceBell		2
