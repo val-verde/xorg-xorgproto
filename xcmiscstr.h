b069e32513c91b90fa326cac4f45606e309224f8
Peter Hutterer <peter.hutterer@who-t.net>
2009-08-26 12:08:58 +1000

    Remove RCS tags
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- xcmiscstr.h
+++ xcmiscstr.h
@@ -1,4 +1,3 @@
-/* $Xorg: xcmiscstr.h,v 1.4 2001/02/09 02:03:24 xorgcvs Exp $ */
 /*
 
 Copyright 1993, 1994, 1998  The Open Group
@@ -24,7 +23,6 @@ used in advertising or otherwise to promote the sale, use or other dealings
 in this Software without prior written authorization from The Open Group.
 
 */
-/* $XFree86$ */
 
 #ifndef _XCMISCSTR_H_
 #define _XCMISCSTR_H_
