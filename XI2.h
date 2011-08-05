4adfb5ad6c064981e2c7eb57db4bdd81cc7029ea
Chase Douglas <chase.douglas@canonical.com>
2011-08-05 15:28:51 -0700

    Specify dependent device pointer/touch handling
    With the added rules, trackpads should be manageable no matter what
    occurs (button presses and pointer motion). Gesture and touch semantics
    during these actions are not well defined, and cancelling touches cleans
    up the protocol and implementation.
    
    Signed-off-by: Chase Douglas <chase.douglas@canonical.com>

--- XI2.h
+++ XI2.h
@@ -157,6 +157,8 @@
 #define XIPointerEmulated                       (1 << 16)
 /* Device event flags (touch events only) */
 #define XITouchPendingEnd                       (1 << 16)
+#define XITouchCanceled                         (1 << 17)
+#define XITouchResumed                          (1 << 18)
 
 /* Touch modes */
 #define XIDirectTouch                           1
